efea8ed9d354ec55a3a711909d628c2ad79dbf36
Brian Sahota <briansahota@Brians-MacBook-Pro.local>
2025-02-10 11:03:28 -0800

    first test complete

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -1,6 +1,8 @@
+#if an incomplete simple string is received the parser should not return a valid frame and not consume any bytes from the buffer
 def test_read_frame_simple_string_incomplete_frame():
   # checks to see if message is complete based on if \r\n is present
   buffer = b"+PartialData"
+  frame, frame_size = extract_frame_from_buffer(buffer)
   # checks to see buffer is not a valid complete message
   assert frame == None
   # checks to see that zero bytes were processed
