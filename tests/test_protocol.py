9da5ad8444b6b3d3ba2e3900a1ea3a2a6b7e4c6e
Brian Sahota <briansahota@macbookpro.mynetworksettings.com>
2025-02-10 18:56:32 -0500

    finished tests for frame extraction and RESP simple string parsing

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -7,3 +7,15 @@ def test_read_frame_simple_string_incomplete_frame():
   assert frame == None
   # checks to see that zero bytes were processed
   assert frame_size == 0
+
+def test_read_frame_simple_string_complete_frame():
+  buffer = b"+OK\r\n"
+  frame, frame_size = extract_frame_from_buffer(buffer)
+  assert frame == SimpleString("OK")
+  assert frame_size == 5
+
+def test_read_frame_simple_string_extra_data():
+  buffer = b"+OK\r\n+Next"
+  frame, frame_size = extract_frame_from_buffer(buffer)
+  assert frame == SimpleString("OK")
+  assert frame_size == 5
